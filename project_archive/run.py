cb81d315ca047a8605dcf07b70035820f1d3a521
Matt Rigby <matt.rigby@bristol.ac.uk>
2024-06-25 09:58:42 +0100

    Minor updates to run file

--- project_archive/run.py
+++ project_archive/run.py
@@ -1,4 +1,10 @@
 from agage_archive.run import run_all
 
+
 if __name__ == "__main__":
-    run_all("project", baseline=False, monthly=False)
+
+    run_all("project",
+            combined=False,
+            baseline=False,
+            monthly=False,
+            top_level_only=True,)
